4327f56c09ea1755feb6cfc9956f5d390344596b
Nicolas Janik <nicolas.janik@gmail.com>
2015-03-03 22:26:30 -0500

    The parser start to work correcty. But the can't be extracted to excel or csv yet

--- ols_parser.py
+++ ols_parser.py
@@ -1,22 +1,27 @@
-
+import sys
 
 class OlsFileParser:
 
     inputFile = ''
-    olsData = None
+    capturedData = None
 
     def __init__(self, inputFile):
 
-
-        self.olsData = OlsData()
         self.inputFile = inputFile
 
-        self.parseHeader()
+        headers = self.parseHeader()
+
+
+
+        trames = self.parseData(headers)
+
 
 
 
 
     def parseHeader(self):
+
+        headers = {}
 
         with open(self.inputFile, "r") as file:
             for line in file:
@@ -33,34 +38,97 @@ class OlsFileParser:
                     value = values[1].strip()
 
                     if fieldname == 'Rate':
-                        self.olsData.rate = value
+                        headers['rate'] = int(value)
 
                     elif fieldname == 'Channels':
-                        self.olsData.channels = value
+                        headers['channels'] = int(value)
 
                     elif fieldname == 'EnabledChannels':
-                        self.olsData.enabledChannels = value
+                        headers['enabledChannels'] = int(value)
 
-        print vars(self.olsData)
-
-
+        return headers
 
 
-class OlsData:
+
+
+    def parseData(self, headers):
+
+        trames = TramesData(headers['rate'], headers['channels'])
+
+        with open(self.inputFile, "r") as file:
+            for line in file:
+                line = line.strip()
+                firstChar = line[:1]
+                if firstChar != ";":
+
+
+
+                    values = line.split('@')
+
+                    channelsState = int(values[0], 16);
+                    time = int(values[1]);
+
+                    values = []
+
+                    mask = 1
+                    for i in range(0, headers['channels']):
+                        channelState = channelsState & mask
+                        mask = mask << 1
+
+                        values.append( channelState > 0  )
+
+                        trames.addData(time, values)
+
+
+        print trames.displayData()
+
+
+
+
+
+
+class TramesData:
 
 
     rate = 0
-    channels = 0
-    enabledChannels = 0
+    channelsNumber = 0
 
 
-    #def __init__(self):
-
-
-
-    def parseHeader(self):
-        return 'hello world'
+    data = {}
 
 
 
-parser = OlsFileParser('ols/1.ols')
+
+
+    def __init__(self, rate, numberOfChannel):
+
+        self.numberOfChannel = numberOfChannel
+        self.rate = rate
+
+        self.data = {}
+
+
+
+
+    def addData(self, time, values):
+
+        self.data[time] = values
+
+
+    def displayData(self):
+
+        for time in sorted(self.data):
+            print time
+            timeInSec = time / float(self.rate)
+            print str(timeInSec)
+            print self.data[time]
+            print '--------'
+
+
+
+
+
+
+
+
+parser = OlsFileParser('ols/fake.ols')
