5dd081a9e6f53e46228cfed4ed0e30b77cda7174
Nicolas Janik <nicolas.janik@gmail.com>
2015-03-04 21:31:34 -0500

    First complete version of the export in xlsx format

--- ols_parser.py
+++ ols_parser.py
@@ -1,9 +1,9 @@
-import sys
+import sys, xlsxwriter
 
 class OlsFileParser:
 
     inputFile = ''
-    capturedData = None
+
 
     def __init__(self, inputFile):
 
@@ -103,7 +103,7 @@ class TramesData:
 
     data = {}
 
-
+    cleanedData = {}
 
 
 
@@ -112,7 +112,7 @@ class TramesData:
         self.numberOfChannel = numberOfChannel
         self.rate = rate
 
-        self.data = {}
+
 
 
 
@@ -120,6 +120,33 @@ class TramesData:
     def addData(self, time, values):
 
         self.data[time] = values
+
+
+    #this method return a dictionnary of the channels values but remove all unncessary entry
+    #example: 0>True  10>true   12> false will be changed with:
+    #           0>True  12>false  (the 10>true is not necessary)
+
+
+    def getCleanData(self, channel):
+
+        cleannedData = {}
+        oldState = False
+        #print self.data
+
+        oldTime = 0
+        for time in sorted(self.data):
+            #print self.data[time][channel]
+            if self.data[time][channel] != oldState:
+
+
+                cleannedData[time] = {'state':oldState, 'duration':time - oldTime}
+
+                oldState = self.data[time][channel]
+                oldTime = time
+
+        return cleannedData
+
+
 
 
     def displayData(self):
@@ -154,59 +181,100 @@ class TramesExporter:
 
         for channel in self.options['channelsToExport']:
 
-            oldState = False
-            stateTime = 0
+            data = self.trames.getCleanData(channel)
 
             convertedTrames = []
             packet = []
 
+            for time in sorted(data):
 
-            for time in sorted(self.trames.data):
-                print time
                 timeInSec = time / float(self.trames.rate)
 
-                state = self.trames.data[time][channel]
-                print str(timeInSec)
-                print self.trames.data[time][channel]
-                print '--------'
+                print str(time)
+                print data[time]
+
+                state = data[time]['state']
+                duration = data[time]['duration']
+
+                #start of a new trame
+                if state == self.options['logicalOneState'] and duration == self.options['startTrameTime']:
+                    packet = []
 
 
-                if state == oldState:
-                    stateTime = stateTime + 1
+                elif state == self.options['logicalOneState'] and duration == self.options['logicalOneTime']:
+                    packet.append(True)
+
+                elif state == self.options['logicalZeroState'] and duration == self.options['logicalZeroTime']:
+                    packet.append(False)
+
+
+                elif duration > self.options['spaceBetweenTrameTime'] and len(packet) > 0:
+                    convertedTrames.append(packet)
+
 
                 else:
-                    #change state
-
-
-                    #start of a new trame
-                    if state == self.options['logicalOneState'] and stateTime == self.options['startTrameTime']:
-                        packet = []
-                        stateTime = 0
-
-                    #ONE
-                    elif state == self.options['logicalOneState'] and stateTime == self.options['logicalOneTime']:
-                        packet.append(True)
-                        stateTime = 0
-
-
-                    #ZERO
-                    elif state == self.options['logicalZeroState'] and stateTime == self.options['logicalZeroTime']:
-                        packet.append(False)
-                        stateTime = 0
-
-                    elif stateTime > self.options['spaceBetweenTrameTime']:
-                        convertedTrames.append(packet)
-                        stateTime = 0
+                    print '*******'
 
 
 
-
-                oldState = state
-
-            print convertedTrames
+            #print convertedTrames
 
 
-parser = OlsFileParser('ols/fake.ols')
+
+            # Create an new Excel file and add a worksheet.
+            workbook = xlsxwriter.Workbook('trames.xlsx')
+            worksheet = workbook.add_worksheet()
+
+            # Widen the first column to make the text clearer.
+            #worksheet.set_column('A:A', 20)
+
+            # Add a bold format to use to highlight cells.
+            #bold = workbook.add_format({'bold': True})
+
+
+
+            # Text with formatting.
+            #worksheet.write('A2', 'World', bold)
+
+            # Write some numbers, with row/column notation.
+            #worksheet.write(2, 0, 123)
+            #worksheet.write(3, 0, 123.456)
+
+
+            worksheet.set_column('A:FF', 1)
+
+            formatZero = workbook.add_format()
+            formatZero.set_font_color('gray')
+
+            formatOne = workbook.add_format()
+            formatOne.set_font_color('black')
+
+            line = 0
+            for packet in convertedTrames:
+
+                col = 0
+                for value in packet:
+
+                    if value == True:
+                        value = '1'
+                        cellFormat = formatOne
+                    else:
+                        value = '0'
+                        cellFormat = formatZero
+
+                    worksheet.write(line, col, value, cellFormat)
+                    col = col + 1
+
+                line = line + 1
+
+
+            # Insert an image.
+            #worksheet.insert_image('B5', 'logo.png')
+
+            workbook.close()
+
+
+parser = OlsFileParser('ols/1.ols')
 trames = parser.getTrames()
 
 exportService = TramesExporter(trames)
